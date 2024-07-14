6044d9121773257481f1b600d96f417ad12d0176
kchranger <kchranger@gmail.com>
2024-07-14 18:53:19 +0900

    save user name to localstorage

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,27 +1,50 @@
-import { useRef, useState } from "react"
+import { useRef, useState,useEffect } from "react"
 import InputName from "./InputName"
 import DisplayName from "./DisplayName"
 import HeaderStyles from '../../css/Header/Header.module.css'
 import DisplayStyles from '../../css/Header/DisplayName.module.css'
 import InputStyles from '../../css/Header/InputName.module.css'
 
+const nameKey = 'ToDoListName'
+
 const Header = () =>{
-  const [name,setName] = useState("")
-  const nameRef = useRef("")
-  const [inputClass,setInputClass] = useState(InputStyles['input-name'])
-  const [displayClass,setDisplayClass] = useState(`${DisplayStyles['display-name']} ${HeaderStyles.hidden}`)
+
+  let localName = localStorage.getItem(nameKey)
+  const [name,setName] = useState(localName)
+  const nameRef = useRef(localName)
+  const [inputClass,setInputClass] = useState()
+  const [displayClass,setDisplayClass] = useState()
+
+  const loadName= () => {
+    if(localName === null || localName === ''){ 
+      setInputClass(InputStyles['input-name'])
+      setDisplayClass(`${DisplayStyles['display-name']} ${HeaderStyles.hidden}`)
+      return
+    }
+
+    setName(localName)
+    setInputClass(`InputStyles['input-name'] ${HeaderStyles.hidden}`)
+    setDisplayClass(`${DisplayStyles['display-name']}`)
+  }
+
 
   const submitName = () =>{
     setName(nameRef.current.value)
     setInputClass(`${InputStyles['input-name']} ${HeaderStyles.hidden}`)
     setDisplayClass(`${DisplayStyles['display-name']}`)
+    localStorage.setItem(nameKey,nameRef.current.value)
   }
 
   const modifyName = () =>{
     setInputClass(`${InputStyles['input-name']}`)
     setDisplayClass(`${DisplayStyles['display-name']} ${HeaderStyles.hidden}`)
+    nameRef.current.value = name
   }
-  
+
+  useEffect(() => {
+    loadName()
+  },[])
+
   return(
     <header className={HeaderStyles.header} >
       <InputName nameRef={nameRef} className={inputClass} submitName={submitName}/>
